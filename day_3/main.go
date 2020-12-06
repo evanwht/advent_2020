67a582df6bcae2450d4a6ad098a66ac11557db9c
Evan White <evanwht1@gmail.com>
2020-12-05 20:44:00 -0600

    day 3 challenge 2

--- day_3/main.go
+++ day_3/main.go
@@ -9,15 +9,24 @@ import (
 
 func main() {
 	treeMap := readMap()
+	count := numTrees(treeMap, 1, 1)
+	count *= numTrees(treeMap, 1, 3)
+	count *= numTrees(treeMap, 1, 5)
+	count *= numTrees(treeMap, 1, 7)
+	count *= numTrees(treeMap, 2, 1)
+	fmt.Println(count)
+}
+
+func numTrees(treeMap [][]byte, slopeX int, slopeY int) int {
 	x, y, maxY, count := 0, 0, len(treeMap[0]), 0
 	for x < len(treeMap) {
 		if treeMap[x][y] == 35 {
 			count++
 		}
-		x++
-		y = (y + 3) % maxY
+		x += slopeX
+		y = (y + slopeY) % maxY
 	}
-	fmt.Println(count)
+	return count
 }
 
 func readMap() (treeMap [][]byte) {
